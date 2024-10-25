243e603536fa0ef4ba8eb2f0ca2a3c8bd64c2b8d
PRASHANT KUMAR JHA <55628661+prashantj510@users.noreply.github.com>
2024-10-25 11:51:52 +0530

    >Meeting params fixed

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+interface MeetingProps {
+  params: {
+    id: string;
+  };
+}
+
+const Meeting: React.FC<MeetingProps> = ({ params }) => {
   return (
     <div>
       Meeting Room : {params.id}
     </div>
-  )
-}
+  );
+};
 
-export default Meeting
+export default Meeting;
